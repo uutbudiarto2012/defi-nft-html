a8d4d983e959c36d1f11d070ea5fe0ee11c996ff
uutbudiarto2012 <hikids.developer@gmail.com>
2021-11-23 12:05:53 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,78 @@
+const menuMobile = document.querySelector('.menuMobile')
+const btnToggleMenu = document.querySelector('.btnToggleMenu')
+const closeSidenav = document.querySelector('.closeSidenav')
+const serachContainer = document.querySelector('.search-container input')
+const sidebarWrapper = document.querySelector('.sidebar-wrapper')
+const toggleSidebar = document.querySelector('.toggleSidebar')
+const openSidebar = document.querySelector('.openSidebar')
+
+toggleSidebar.addEventListener('click',function(){
+  sidebarWrapper.classList.remove('d-lg-flex')
+  openSidebar.classList.remove('d-none')
+})
+openSidebar.addEventListener('click',function(){
+  sidebarWrapper.classList.add('d-lg-flex')
+  openSidebar.classList.add('d-none')
+})
+
+btnToggleMenu.addEventListener('click',function () {
+  menuMobile.classList.add('show')
+})
+closeSidenav.addEventListener('click',function () {
+  menuMobile.classList.remove('show')
+})
+
+serachContainer.addEventListener('focus',function () {
+  this.parentElement.classList.add('focused')
+})
+serachContainer.addEventListener('blur',function () {
+  this.parentElement.classList.remove('focused')
+})
+
+
+
+
+$('._notable-wrapper').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows:true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows:false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        centerMode: true,
+        infinite: true,
+        centerPadding: '60px',
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        centerMode: true,
+        infinite: true,
+        centerPadding: '20px',
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
